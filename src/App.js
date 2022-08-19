1b6753f8f7e9a61e070eae51fbe4999f41f653ac
Vit Onischenko <slonik0775@gmail.com>
2022-08-19 15:56:39 +0300

    реализовал вывод пользователей + фильтрацию по имени и емейлу

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
@@ -6,9 +6,29 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+    const [users, setUsers] = useState([])
+    const [isLoading, setLoading] = useState(true)
+    const [searchValue, setSearchValue] = useState('')
+
+useEffect(() => {
+fetch('https://reqres.in/api/users')
+    .then((res)=>res.json())
+    .then((json) => {
+    setUsers(json.data);
+})
+    .catch((err)=> {
+    console.warn(err)
+    alert('Ошибка при использовании')
+}).finally(() => setLoading(false))
+}, [])
+
+    const onChangeSearchValue = (event) => {
+        setSearchValue(event.target.value)
+    }
+
   return (
     <div className="App">
-      <Users />
+      <Users searchValue={searchValue} items={users} isLoading={isLoading} onChangeSearchValue={onChangeSearchValue} />
       {/* <Success /> */}
     </div>
   );
